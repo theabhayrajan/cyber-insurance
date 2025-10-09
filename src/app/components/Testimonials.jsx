2a4971b471a6c8297199b0772928e4d5611315da
theabhayrajan <abhayrajan9922@gmail.com>
2025-10-09 11:47:28 -0800

    testimonials cards

--- src/app/components/Testimonials.jsx
+++ src/app/components/Testimonials.jsx
@@ -39,32 +39,32 @@ export default function Testimonials() {
         <div className="flex flex-col lg:flex-row items-center justify-center gap-4 mb-10">
           {/* Rating */}
           <div className="flex items-center gap-2">
-            <span className="text-black dark:text-white text-xl lg:text-[1.1rem]">★★★★★</span>
-            <p className="text-lg text-black dark:text-white lg:text-[1.1rem]">4.9/5 average rating</p>
+            <span className="text-black dark:text-white text-[1rem] sm:text-xl lg:text-[1.1rem]">★★★★★</span>
+            <p className="text-[1rem] text-black dark:text-white md:text-[1.1rem]">4.9/5 average rating</p>
           </div>
 
           {/* Used by 1000+ */}
           <div className="flex items-center gap-2">
             <UsersIcon className="h-5 w-5 text-black dark:text-white lg:text-[1.1rem]" />
-            <p className="text-lg text-black dark:text-white lg:text-[1.1rem]">Used by 1000+ SMBs</p>
+            <p className="text-[1rem] text-black dark:text-white lg:text-[1.1rem]">Used by 1000+ SMBs</p>
           </div>
 
           {/* Backed by cybersecurity */}
-          <div className="flex items-center gap-2 max-w-xs sm:max-w-none text-center sm:text-left">
-            <ShieldCheckIcon className="shrink-0 h-5 w-5 sm:h-6 sm:w-6 md:h-8 md:w-8 lg:h-7 text-black dark:text-white" />
-            <p className="text-[0.9rem] sm:text-sm md:text-lg text-black dark:text-white lg:text-[1.1rem]">
+          <div className="flex items-center gap-1 max-w-xs sm:max-w-none text-center sm:text-left">
+            <ShieldCheckIcon className="shrink-0 h-5 w-5 sm:h-6 sm:w-6 md:h-6 md:w-8 lg:h-7 text-black dark:text-white" />
+            <p className="text-[0.85rem] sm:text-sm md:text-lg text-black dark:text-white lg:text-[1.1rem]">
               Backed by cybersecurity and insurance experts
             </p>
           </div>
         </div>
 
         {/* Testimonials Marquee */}
-        <div className="mt-20 overflow-hidden">
-          <Marquee pauseOnHover={false} gradient={false} speed={50}>
+        <div className="overflow-hidden">
+          <Marquee pauseOnHover={false} gradient={false} speed={50} className="h-[270px]">
             {testimonials.map((testimonial, index) => (
               <div
                 key={index}
-                className="bg-white p-6 rounded-lg shadow-lg transform transition-all duration-300 mx-5 w-full max-w-sm"
+                className="bg-white p-6 rounded-lg shadow-lg transform transition-all duration-300 mx-5 w-90 h-60 max-w-sm"
               >
                 <div className="flex items-center mb-4">
                   <img
@@ -73,11 +73,11 @@ export default function Testimonials() {
                     className="w-16 h-16 rounded-full object-cover mr-4"
                   />
                   <div>
-                    <h3 className="text-xl font-semibold text-gray-800">{testimonial.name}</h3>
-                    <p className="text-sm text-gray-500">{testimonial.role}</p>
+                    <h3 className="text-[1.7rem] md:text-[1.8rem] xl:text-[1.9rem] text-gray-800">{testimonial.name}</h3>
+                    <p className="text-[1rem] md:text-[1.1rem] ml-1 text-gray-500 self-start flex">{testimonial.role}</p>
                   </div>
                 </div>
-                <p className="text-gray-700 italic">"{testimonial.quote}"</p>
+                <p className="text-[#364152] md:text-[1.1rem] text-left ">"{testimonial.quote}"</p>
               </div>
             ))}
           </Marquee>
